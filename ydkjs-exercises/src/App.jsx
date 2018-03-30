0145d217c9aea2059c810d07641b265048eabcd2
austintackaberry <austintackaberry@gmail.com>
2018-03-30 11:58:56 -0700

    Add fade to home page

--- ydkjs-exercises/src/App.jsx
+++ ydkjs-exercises/src/App.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import UpGoing from './components/UpGoing';
+import Fade from 'react-reveal/Fade';
 import Home from './components/Home';
 
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
-      <h1 style={{ fontSize: '55px' }}>YDKJS EXERCISES</h1>
-    </Link>
-    <Route exact path="/" component={Home} />
-    <Route path="/up-going" component={UpGoing} />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
+          <h1 style={{ fontSize: '55px' }}>YDKJS EXERCISES</h1>
+        </Link>
+        <Fade right opposite>
+          <Route exact path="/" component={Home} />
+          <Route path="/up-going" component={UpGoing} />
+        </Fade>
+      </div>
+    );
+  }
+}
 
 export default App;
