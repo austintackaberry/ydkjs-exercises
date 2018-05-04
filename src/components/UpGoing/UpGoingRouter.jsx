903a9ce8804992205857e2e49ee90e2a65b98ea2
austintackaberry <austintackaberry@gmail.com>
2018-05-04 00:31:13 -0700

    Made 2nd header a link

--- src/components/UpGoing/UpGoingRouter.jsx
+++ src/components/UpGoing/UpGoingRouter.jsx
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import UpGoingCh1 from "./UpGoingCh1.jsx";
 import UpGoingCh2 from "./UpGoingCh2.jsx";
 import UpGoingCh3 from "./UpGoingCh3.jsx";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 class UpGoingRouter extends Component {
   render() {
     const { match } = this.props;
     return (
       <div>
-        <h2 style={{ fontSize: "40px" }}>Up & Going</h2>
+        <Link style={{ textDecoration: "none", color: "black" }} to="/up-going">
+          <h2 style={{ fontSize: "40px" }}>Up & Going</h2>
+        </Link>
         <Route path={match.url + "/ch1"} component={UpGoingCh1} />
         <Route path={match.url + "/ch2"} component={UpGoingCh2} />
         <Route path={match.url + "/ch3"} component={UpGoingCh3} />
