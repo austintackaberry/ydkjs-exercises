a6c9c21929b52d1a040d95a48db858f5e2f96c2b
elloo <ewelinloo@gmail.com>
2018-05-18 13:43:14 +1000

    18/05/2018 Added questions to Up & Going Ch 1

--- src/data/UpGoing/ch1.js
+++ src/data/UpGoing/ch1.js
@@ -19,6 +19,47 @@ const Ch1Questions = [
         isCorrect: true
       }
     ]
+  },
+  {
+    question: "What is a computer program (source code / code)?",
+    answers: [
+      {
+        answer: "A set of special instructions to tell the computer what tasks to perform.",
+        isCorrect: true
+      },
+      {
+        answer: "A planned series of future events or performances.",
+        isCorrect: false
+      },
+      {
+        answer: "A sheet or booklet giving details of items or performers at an event or performance.",
+        isCorrect: false
+      },
+      {
+        answer: "A plan of action to accomplish a specified end.",
+        isCorrect: false
+      }
+    ]
+  },
+  {
+    question: "What is a computer language (syntax)?",
+    answers:
+    {
+      answer: "The arrangement of words and phrases to create well-formed sentences.",
+      isCorrect: false
+    }
+    {
+      answer: "The rules for valid format and combinations of instructions.",
+      isCorrect: true
+    }
+    {
+      answer: "The use of words in a structured and conventional way.",
+      isCorrect: false
+    }
+    {
+      answer: "A system of communication",
+      isCorrect: false
+    }
   }
 ];
 
