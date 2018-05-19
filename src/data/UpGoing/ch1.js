5613406356b4d20aec48341288c94ac50a76d583
elloo <ewelinloo@gmail.com>
2018-05-19 18:11:43 +1000

    Added three new questions

--- src/data/UpGoing/ch1.js
+++ src/data/UpGoing/ch1.js
@@ -61,6 +61,69 @@ const Ch1Questions = [
         isCorrect: false
       }
     ]
+  },
+  {
+    question: "What is an expression?",
+    answers: [
+      {
+        answer: "The action of making known one's thoughts or feelings.",
+        isCorrect: false
+      },
+      {
+        answer: "Values that are included directly in the source code.",
+        isCorrect: false
+      },
+      {
+        answer: "Any reference to a variable or value OR a set of variable(s) and value(s) combined with operators.",
+        isCorrect: true
+      },
+      {
+        answer: "A statement that performs actions with values and variables.",
+        isCorrect: false
+      }
+    ]
+  },
+  {
+    question: "Interpreting the code is when... ?",
+    answers: [
+      {
+        answer: "Changes to values are tracked as your program runs.",
+        isCorrect: false
+      },
+      {
+        answer: "Actions are performed with values and variables such as assignment and mathematic multiplication.",
+        isCorrect: false
+      },
+      {
+        answer: "The translation of commands is done ahead of time so when the program is executed, what's running is the already translated computer instructions.",
+        isCorrect: false
+      },
+      {
+        answer: "Translation of commands is typically done from top to bottom, line by line, every time the program is run.",
+        isCorrect: true
+      }
+    ]
+  },
+  {
+    question: "Compiling is when... ?",
+    answers: [
+      {
+        answer: "The translation of commands is done ahead of time so when the program is executed, what's running is the already translated computer instructions.",
+        isCorrect: true
+      },
+      {
+        answer: "Changes to values are tracked as your program runs.",
+        isCorrect: false
+      },
+      {
+        answer: "Actions are performed with values and variables such as assignment and mathematic multiplication.",
+        isCorrect: false
+      },
+      {
+        answer: "Translation of commands is typically done from top to bottom, line by line, every time the program is run.",
+        isCorrect: false
+      }   
+    ]
   }
 ];
 
