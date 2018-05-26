5cdc7802f85e15c1f73d5827cf96d014de4d6139
Chris <Christopher.D.Culp@gmail.com>
2018-05-26 17:30:11 -0400

    Added Footer component, hitched it to App.js with contribute message

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "../App.css"
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="Footer">
+            
+                < p > YDKJS Exercises is created and maintained by learners like you! To take a look at the source code or contribute to the project, check out the repository at < a href = "https://github.com/austintackaberry/ydkjs-exercises"> https://github.com/austintackaberry/ydkjs-exercises</a></p>
+            </div>            
+        );
+    }
+}
+
+export default Footer
