a5fd3caeff43af33933c93ee454a8e3c8f5a1bce
austintackaberry <austintackaberry@gmail.com>
2018-05-29 01:07:02 -0700

    Sticky footer to bottom of page

--- src/App.js
+++ src/App.js
@@ -10,19 +10,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link style={{ textDecoration: "none", color: "black" }} to="/">
-          <h1 style={{ fontSize: "55px" }}>YDKJS EXERCISES</h1>
-        </Link>
-        <Route exact path="/" render={() => <Home books={books} />} />
-        {books.map(book => {
-          return (
-            <Route
-              key={book.id}
-              path={book.url}
-              render={() => <BookRouter book={book} />}
-            />
-          );
-        })}
+        <div className="main-content">
+          <Link style={{ textDecoration: "none", color: "black" }} to="/">
+            <h1 style={{ fontSize: "55px" }}>YDKJS EXERCISES</h1>
+          </Link>
+          <Route exact path="/" render={() => <Home books={books} />} />
+          {books.map(book => {
+            return (
+              <Route
+                key={book.id}
+                path={book.url}
+                render={() => <BookRouter book={book} />}
+              />
+            );
+          })}
+        </div>
         <Footer />
       </div>
     );
