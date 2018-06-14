1940556204de935d2f0e284cc6ab16ae3d95ae82
Reuben Reyes <reuben.a.reyes@gmail.com`>
2018-06-13 21:46:16 -0700

    map key "id" to each book

--- src/data/index.js
+++ src/data/index.js
@@ -5,43 +5,42 @@ import TypesGrammarChapters from './TypesGrammar';
 import AsyncPerformanceChapters from './AsyncPerformance';
 import ES6BeyondChapters from './ES6Beyond';
 
-const books = [
+const booksNoId = [
   {
-    id: 0,
     url: '/up-going',
     title: 'Up & Going',
     chapters: UpGoingChapters,
   },
   {
-    id: 1,
     url: '/scope-closures',
     title: 'Scope & Closures',
     chapters: ScopeClosuresChapters,
   },
   {
-    id: 2,
     url: '/this-object-prototypes',
     title: 'this & Object Prototypes',
     chapters: ThisObjectPrototypesChapters,
   },
   {
-    id: 3,
     url: '/types-grammar',
     title: 'Types & Grammar',
     chapters: TypesGrammarChapters,
   },
   {
-    id: 4,
     url: '/async-performance',
     title: 'Async & Performance',
     chapters: AsyncPerformanceChapters,
   },
   {
-    id: 5,
     url: '/es6-beyond',
     title: 'ES6 & Beyond',
     chapters: ES6BeyondChapters,
   },
 ];
+
+const books = booksNoId.map((book, i) => {
+  book.id = i;
+  return book;
+});
 
 export default books;
