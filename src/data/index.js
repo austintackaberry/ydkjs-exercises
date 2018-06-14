e28aa895d7da73aae0bafc7ab07b7f43acaae6ea
Reuben Reyes <reuben.a.reyes@gmail.com`>
2018-06-13 21:22:38 -0700

    add key "id" to each book

--- src/data/index.js
+++ src/data/index.js
@@ -7,31 +7,37 @@ import ES6BeyondChapters from './ES6Beyond';
 
 const books = [
   {
+    id: 0,
     url: '/up-going',
     title: 'Up & Going',
     chapters: UpGoingChapters,
   },
   {
+    id: 1,
     url: '/scope-closures',
     title: 'Scope & Closures',
     chapters: ScopeClosuresChapters,
   },
   {
+    id: 2,
     url: '/this-object-prototypes',
     title: 'this & Object Prototypes',
     chapters: ThisObjectPrototypesChapters,
   },
   {
+    id: 3,
     url: '/types-grammar',
     title: 'Types & Grammar',
     chapters: TypesGrammarChapters,
   },
   {
+    id: 4,
     url: '/async-performance',
     title: 'Async & Performance',
     chapters: AsyncPerformanceChapters,
   },
   {
+    id: 5,
     url: '/es6-beyond',
     title: 'ES6 & Beyond',
     chapters: ES6BeyondChapters,
