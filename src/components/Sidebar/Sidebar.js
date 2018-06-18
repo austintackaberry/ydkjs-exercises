08a98d1dd90c80b748e6e09de9bb2ec28b09c925
nikrb <nik_rb@yahoo.com>
2018-06-18 17:57:40 +0100

    stylify Sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { List, ListItem, ListItemTitle, Divider, Wrapper } from './styled';
+import { Link } from 'react-router-dom';
+
+class Sidebar extends Component {
+  render() {
+    const books = this.props.books;
+    const score = this.props.score;
+    const currentScore = score.current;
+    const possibleScore = score.possible;
+
+    const scoreDisplay = `You've answered ${currentScore} of ${possibleScore} questions correctly. Time to dive in!`;
+
+    return (
+      <Wrapper>
+        <List>
+          <ListItemTitle>Progress</ListItemTitle>
+          <ListItem>{scoreDisplay}</ListItem>
+
+          <Divider />
+
+          <ListItemTitle>Books</ListItemTitle>
+          {books.map((book, index) => (
+            <Link
+              key={book.id}
+              style={{ textDecoration: 'none' }}
+              to={book.url}
+            >
+              <ListItem>
+                {`${book.title} (${score.books[index].current}/
+                  ${score.books[index].possible})`}
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Wrapper>
+    );
+  }
+}
+
+export default Sidebar;
---
+++ src/components/Sidebar/styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Divider = styled.hr`
+  height: 12px;
+  border: 0;
+  box-shadow: inset 0 12px 12px -12px rgba(0, 0, 0, 0.5);
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  width: 100%;
+  margin: 0 auto;
+  padding: 1rem 0;
+`;
+
+export const ListItemTitle = styled.li`
+  list-style: none;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 16px;
+  padding: 0.5rem 0;
+`;
+export const ListItem = styled.li`
+  list-style: none;
+  font-size: 12px;
+  line-height: 12px;
+  padding: 0.5rem 0.3rem;
+`;
+
+export const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: white;
+  width: 15vw;
+  height: 100%;
+  z-index: 99999;
+`;
