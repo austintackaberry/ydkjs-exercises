9bbfc1e5daa6c0a93933bbe66aea6f8923c0949c
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-06-30 17:09:55 -0700

    change score display text. add ProgressBar rendering

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { List, ListItem, ListItemTitle, Divider, Wrapper } from './styled';
 import { Link } from 'react-router-dom';
+import ProgressBar from '../ProgressBar';
 
 class Sidebar extends Component {
   static propTypes = {
@@ -11,16 +12,28 @@ class Sidebar extends Component {
   render() {
     const books = this.props.books;
     const score = this.props.score;
-    const currentScore = score.current;
-    const possibleScore = score.possible;
-
-    const scoreDisplay = `You've answered ${currentScore} of ${possibleScore} questions correctly. Time to dive in!`;
+    const scoreAnswered = score.correct + score.incorrect;
+    const scorePct = Math.round((100 * score.correct) / scoreAnswered) || 0;
+    // const scoreDisplay =
+    //   `You've answered ${ score.correct } out of ${ scoreAnswered } (${ scorePct }%). ${ score.possible - scoreAnswered } left to answer.`;
 
     return (
       <Wrapper>
         <List>
           <ListItemTitle>Progress</ListItemTitle>
-          <ListItem>{scoreDisplay}</ListItem>
+          <ListItem>
+            <ProgressBar score={score} />
+          </ListItem>
+          <ListItem>
+            <p>
+              You've answered <strong>{score.correct}</strong> out of{' '}
+              <strong>{scoreAnswered}</strong> (<strong>{scorePct}%</strong>)
+              questions correctly.
+            </p>
+            <p>
+              <strong>{score.possible - scoreAnswered}</strong> left to answer.
+            </p>
+          </ListItem>
 
           <Divider />
 
@@ -32,7 +45,7 @@ class Sidebar extends Component {
               to={book.url}
             >
               <ListItem>
-                {`${book.title} (${score.books[index].current}/
+                {`${book.title} (${score.books[index].correct}/
                   ${score.books[index].possible})`}
               </ListItem>
             </Link>
