19e89ea8bcc0bcbbd2d99cab71a9f9806997d38a
austintackaberry <austintackaberry@gmail.com>
2018-08-11 01:49:50 -0700

    test(data): Ensure that each questionId has length of 20

--- src/__tests__/data.js
+++ src/__tests__/data.js
@@ -1,12 +1,13 @@
 import books from '../data';
 import { createScoreMap } from '../helpers/helpers';
 
-it('Should have a unique id for each question', () => {
+it('Should have a 20 char unique id for each question', () => {
   const score = { books };
   const scoreMap = createScoreMap(score);
   const counter = books.reduce((acc, book) => {
     book.chapters.forEach(chapter => {
       chapter.questions.forEach(question => {
+        expect(question.questionId).toHaveLength(20);
         acc++;
       });
     });
