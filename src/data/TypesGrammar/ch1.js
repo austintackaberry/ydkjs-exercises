0fe0ca284471c876572db363303fe958ae3d6028
austintackaberry <austintackaberry@gmail.com>
2018-08-13 02:34:34 -0700

    feat(TypesGrammar/ch1): Complete questions

--- src/data/TypesGrammar/ch1.js
+++ src/data/TypesGrammar/ch1.js
@@ -2,7 +2,7 @@ const Ch1Questions = [
   {
     question: 'Which is a valid JavaScript type for numerical data?',
     questionId: 'pMUUWcGwln5koHSPych2',
-    shouldBeRandomized: false,
+    shouldBeRandomized: true,
     answers: [
       { text: 'int', id: 0 },
       { text: 'short', id: 1 },
@@ -33,49 +33,67 @@ const Ch1Questions = [
       'In JavaScript, arrays are a type of `object`. There is no "array" type.',
   },
   {
-    question: '',
+    question: `What will be logged to the console when the following code is executed:
+${'```js'}
+var a = null;
+
+console.log(!a && typeof a === "object");
+${'```'}  
+    `,
     questionId: 'JusfVHYhYwoarDGU2aMo',
-    shouldBeRandomized: true,
+    shouldBeRandomized: false,
     answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
+      { text: 'true', id: 0 },
+      { text: 'false', id: 1 },
+      { text: 'undefined', id: 2 },
+      { text: 'TypeError', id: 3 },
     ],
     correctAnswerId: 0,
     moreInfoUrl:
-      'https://github.com/getify/You-Dont-Know-JS/blob/master/types%20%26%20grammar/',
-    explanation: '',
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/types%20%26%20grammar/ch1.md#built-in-types',
+    explanation: `It would have been nice if \`typeof null\` returned "null", but this original bug in JS has persisted for nearly two decades, and will likely never be fixed because there's too much existing web content that relies on its buggy behavior that "fixing" the bug would create more "bugs" and break a lot of web software.`,
   },
   {
-    question: '',
+    question: `What will be logged to the console when the following code is executed:
+${'```js'}
+function a(b, c) {
+  var d = 5
+  return d + 1
+};
+
+console.log(a.length);
+${'```'}  
+    `,
     questionId: 'RVhgX62FTiCZoX7gswGE',
-    shouldBeRandomized: true,
+    shouldBeRandomized: false,
     answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
+      { text: '`undefined`', id: 0 },
+      { text: '`TypeError`', id: 1 },
+      { text: '`1`', id: 2 },
+      { text: '`2`', id: 3 },
     ],
-    correctAnswerId: 0,
+    correctAnswerId: 3,
     moreInfoUrl:
-      'https://github.com/getify/You-Dont-Know-JS/blob/master/types%20%26%20grammar/',
-    explanation: '',
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/types%20%26%20grammar/ch1.md#built-in-types',
+    explanation:
+      'The function object has a length property set to the number of formal parameters it is declared with.',
   },
   {
-    question: '',
+    question:
+      'What is a good way to safely check if a variable has been declared?',
     questionId: 'vcCj5uxXMet4CnxLW96V',
     shouldBeRandomized: true,
     answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
+      { text: '`if (a)...`', id: 0 },
+      { text: '`if(typeof a !== "undefined")...`', id: 1 },
+      { text: '`if(a !== undefined)...`', id: 2 },
+      { text: '`if(a.isDefined())...`', id: 3 },
     ],
-    correctAnswerId: 0,
+    correctAnswerId: 1,
     moreInfoUrl:
-      'https://github.com/getify/You-Dont-Know-JS/blob/master/types%20%26%20grammar/',
-    explanation: '',
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/types%20%26%20grammar/ch1.md#typeof-undeclared',
+    explanation:
+      'All of the other options will throw an error if `a` has not been declared.',
   },
 ];
 
