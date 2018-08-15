77e29c5a6d016b64b8a67ca8b4ccd84e6dca4973
austintackaberry <austintackaberry@gmail.com>
2018-08-15 03:04:07 -0700

    feat(AsyncPerformance): Write questions for ch1-4

--- src/data/AsyncPerformance/ch1.js
+++ src/data/AsyncPerformance/ch1.js
@@ -1,88 +1,77 @@
 const Ch1Questions = [
   {
-    question: `What will be logged to the console when the following code is executed:
-${'```js'}
-${'```'}
-        `,
+    question: `\`console.log()\` is always executed synchronously`,
     questionId: 'DezUGg37iH44NXzdLo3i',
-    shouldBeRandomized: true,
-    answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
-    ],
-    correctAnswerId: 0,
-    moreInfoUrl: '',
-    explanation: '',
+    shouldBeRandomized: false,
+    answers: [{ text: 'True', id: 0 }, { text: 'False', id: 1 }],
+    correctAnswerId: 1,
+    moreInfoUrl:
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch1.md#async-console',
+    explanation:
+      'While it is up to the hosting environment how they want to implement `console`, it is often asynchronous because blocking I/O takes a lot of time.',
   },
   {
     question: `What will be logged to the console when the following code is executed:
 ${'```js'}
+var data = ajax( "https://google.com" );
+
+console.log( data );
 ${'```'}
         `,
     questionId: 'BpY4KEBuBBdeQBlxA3LG',
     shouldBeRandomized: true,
     answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
+      { text: 'The html for google.com', id: 0 },
+      { text: '`undefined`', id: 1 },
     ],
-    correctAnswerId: 0,
-    moreInfoUrl: '',
-    explanation: '',
+    correctAnswerId: 1,
+    moreInfoUrl:
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch1.md#a-program-in-chunks',
+    explanation:
+      'Because `ajax` is asynchronous, `data` will be undefined will not yet be defined when it is logged to the console.',
   },
   {
-    question: `What will be logged to the console when the following code is executed:
-${'```js'}
-${'```'}
-        `,
+    question: `Until ES6, JavaScript itself has actually never had any direct notion of asynchrony built into it.`,
     questionId: 'vjpxPlelT1ljEtKFO90e',
-    shouldBeRandomized: true,
-    answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
-    ],
+    shouldBeRandomized: false,
+    answers: [{ text: 'True', id: 0 }, { text: 'False', id: 1 }],
     correctAnswerId: 0,
-    moreInfoUrl: '',
-    explanation: '',
+    moreInfoUrl:
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch1.md#event-loop',
+    explanation:
+      'What you typically think of asynchrony is often the Web APIs, not the JavaScript engine.',
   },
   {
-    question: `What will be logged to the console when the following code is executed:
-${'```js'}
-${'```'}
-        `,
+    question: `What data structure best explains the event loop?`,
     questionId: 'm6QM7gCTkPglIfbbPGVX',
     shouldBeRandomized: true,
     answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
+      { text: 'stack', id: 0 },
+      { text: 'queue', id: 1 },
+      { text: 'binary tree', id: 2 },
+      { text: 'graph', id: 3 },
     ],
-    correctAnswerId: 0,
-    moreInfoUrl: '',
-    explanation: '',
+    correctAnswerId: 1,
+    moreInfoUrl:
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch1.md#event-loop',
+    explanation:
+      'The event loop is FIFO, or first-in-first-out, which is similar to that of a queue',
   },
   {
-    question: `What will be logged to the console when the following code is executed:
-${'```js'}
-${'```'}
-        `,
+    question: `What statement about \`setTimeout(callbackFn, 1000)\` below is the most accurate.`,
     questionId: '0IR8uGxobATrQNdrpC4g',
     shouldBeRandomized: true,
     answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
+      { text: '`callbackFn` will be executed once 1000ms has elapsed.', id: 0 },
+      { text: '`callbackFn` will be executed in at least 1000ms.', id: 1 },
+      { text: '`callbackFn` will be executed in at most 1000ms.', id: 2 },
       { text: '', id: 3 },
     ],
-    correctAnswerId: 0,
-    moreInfoUrl: '',
-    explanation: '',
+    correctAnswerId: 1,
+    moreInfoUrl:
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch1.md#event-loop',
+    explanation:
+      'Once the timer is up, the callback will get added to the event loop. If the event loop has 20 items, then the callback will execute once those 20 items are executed.',
   },
 ];
 export default Ch1Questions;
--- src/data/AsyncPerformance/ch5.js
+++ src/data/AsyncPerformance/ch5.js
@@ -1,20 +1,22 @@
 const Ch5Questions = [
   {
-    question: `What will be logged to the console when the following code is executed:
-  ${'```js'}
-  ${'```'}
-          `,
+    question: `Why do we care about asynchrony? Why not make everything synchronous?`,
     questionId: 'fSSc7ml4RBdQfi3VnoUO',
     shouldBeRandomized: true,
     answers: [
-      { text: '', id: 0 },
-      { text: '', id: 1 },
-      { text: '', id: 2 },
-      { text: '', id: 3 },
+      { text: 'That would make our code less readable', id: 0 },
+      { text: 'For performance reasons', id: 1 },
+      {
+        text: 'It would not be possible to make everything synchronous',
+        id: 2,
+      },
+      { text: 'JavaScript is moving towards removing asynchrony', id: 3 },
     ],
-    correctAnswerId: 0,
-    moreInfoUrl: '',
-    explanation: '',
+    correctAnswerId: 1,
+    moreInfoUrl:
+      'https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch5.md#chapter-5-program-performance',
+    explanation:
+      'Our code would be very slow if all of our requests were blocking.',
   },
   {
     question: `What will be logged to the console when the following code is executed:
