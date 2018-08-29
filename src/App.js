2bde34c76da2b49022466d3fba737dbd763804e7
austintackaberry <austintackaberry@gmail.com>
2018-08-29 01:30:06 -0700

    feat(public): Add new, larger icons
    #146

--- src/App.js
+++ src/App.js
@@ -30,6 +30,8 @@ class App extends Component {
       score,
       shouldShowSidebar,
       isNarrowScreen,
+      showInstallBtn: false,
+      deferredPrompt: null,
     };
   }
   componentDidMount() {
@@ -43,6 +45,14 @@ class App extends Component {
       });
     }
     this.setState({ score });
+
+    window.addEventListener('beforeinstallprompt', e => {
+      this.setState({ showInstallBtn: true });
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.setState({ deferredPrompt: e });
+    });
   }
 
   handleMenuClick(event) {
@@ -59,8 +69,25 @@ class App extends Component {
     });
   }
 
+  handlePwaInstallClick(event) {
+    // hide our user interface that shows our A2HS button
+    this.setState({ showInstallBtn: false });
+    const { deferredPrompt } = this.state;
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      this.setState({ deferredPrompt: null });
+    });
+  }
+
   render() {
-    const { shouldShowSidebar, isNarrowScreen } = this.state;
+    const { shouldShowSidebar, isNarrowScreen, showInstallBtn } = this.state;
     const overlayStyle =
       shouldShowSidebar && isNarrowScreen
         ? { backgroundColor: 'rgba(0,0,0,0.5)' }
@@ -73,12 +100,18 @@ class App extends Component {
         : {};
     const mainContentStyle =
       shouldShowSidebar && isNarrowScreen ? { pointerEvents: 'none' } : {};
+
+    const pwaInstallBtnStyle = showInstallBtn ? {} : { display: 'none' };
     return (
       <ScoreContext.Provider
         value={{ score: this.state.score, updateScore: this.updateScore }}
       >
         <div id="overlay" style={overlayStyle}>
           <div className="App" style={appMargin}>
+            <button
+              onClick={e => this.handlePwaInstallClick(e)}
+              style={pwaInstallBtnStyle}
+            />
             {shouldShowSidebar && (
               <Sidebar
                 books={books}
