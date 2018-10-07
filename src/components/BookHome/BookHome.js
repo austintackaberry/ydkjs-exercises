47fb29c9b4cbcfd3db162dca1ac35ef3451738fa
esthercuan <esthercuan@gmail.com>
2018-10-07 14:27:46 -0700

    remove unecessary style declaration

--- src/components/BookHome/BookHome.js
+++ src/components/BookHome/BookHome.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { List, ListItem, StyledLink } from './styled';
-import styled from 'styled-components';
 
 class BookHome extends Component {
   static propTypes = {
