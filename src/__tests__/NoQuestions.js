9d84c0d5bce817c9bb085851af3586a20369a28f
austintackaberry <austintackaberry@gmail.com>
2018-10-17 00:02:28 -0700

    Convert NoQuestions to rtl

--- src/__tests__/NoQuestions.js
+++ src/__tests__/NoQuestions.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 
 import NoQuestions from '../components/NoQuestions';
 
 it('should render NoQuestions', () => {
-  const comp = shallow(<NoQuestions />);
-  expect(comp.find('p').length).toBe(2);
-  expect(comp
-    .find('p')
-    .at(0)
-    .text()
-  ).toBe('There are currently no questions for this chapter :(,'+
-    ' but you can change that!');
-  expect(comp
-    .find('p')
-    .at(1)
-    .text()
-  ).toBe('Feel free to submit a pull request to this project here.');
-  expect(comp
-    .find('a')
-    .at(0)
-    .prop('href')
-  ).toBe('https://github.com/austintackaberry/ydkjs-exercises');
+  const { getByText } = render(<NoQuestions />);
+  expect(
+    getByText(
+      'There are currently no questions for this chapter :(, but you can change that!'
+    )
+  ).toBeTruthy();
 });
