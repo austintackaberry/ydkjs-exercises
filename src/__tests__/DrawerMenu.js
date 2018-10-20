049a6e9cda4c1bdd24250ea43420fb11c7f1357e
austintackaberry <austintackaberry@gmail.com>
2018-10-20 16:12:00 -0700

    Convert DrawerHome test to RTL

--- src/__tests__/DrawerMenu.js
+++ src/__tests__/DrawerMenu.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { renderWithRouter } from 'test-utils';
 
 import DrawerMenu from '../components/DrawerMenu';
 
+const props = {
+  children: () => {},
+  id: 'id',
+  to: '/url',
+  title: 'fake-title',
+  nest: 1,
+};
+
 it('should render without breaking', () => {
-  const comp = shallow(<DrawerMenu id="0" to="" title="" nest={1} />);
-  expect(comp).toMatchSnapshot();
+  const { getByText, getByTestId } = renderWithRouter(
+    <DrawerMenu {...props} />
+  );
+  const el = getByText(props.title);
+  const url = el.href.split('/');
+  const pathname = `/${url[url.length - 1]}`;
+  expect(pathname).toBe(props.to);
+  expect(getByTestId(`${props.id}-children`)).toBeTruthy();
 });
