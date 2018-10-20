76c0507a059dad0c89dd503b83d4b254d180dc5d
austintackaberry <austintackaberry@gmail.com>
2018-10-19 23:52:44 -0700

    Convert NoQuestions to rtl

--- src/__tests__/NoMatch.js
+++ src/__tests__/NoMatch.js
@@ -1,24 +1,9 @@
-import React from "react";
-import { shallow } from "enzyme";
+import React from 'react';
+import { renderWithRouter } from 'test-utils';
 
-import NoMatch from "../components/NoMatch";
+import NoMatch from '../components/NoMatch';
 
-it("should render NoMatch", () => {
-  const comp = shallow(<NoMatch />);
-  expect(comp.find("h1").length).toBe(1);
-  expect(
-    comp
-      .find("h1")
-      .at(0)
-      .childAt(0)
-      .text()
-  ).toBe("Ooops");
-  expect(comp.find("p").length).toBe(1);
-  expect(
-    comp
-      .find("p")
-      .at(0)
-      .childAt(0)
-      .text()
-  ).toBe(`Looks like this page doesn't exist`);
+it('should render NoMatch', () => {
+  const { getByText } = renderWithRouter(<NoMatch />);
+  expect(getByText("Looks like this page doesn't exist")).toBeTruthy();
 });
