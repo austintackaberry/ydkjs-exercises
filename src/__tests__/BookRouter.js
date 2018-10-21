6f01b52168f3a36374a5e63d025b987b15d85563
austintackaberry <austintackaberry@gmail.com>
2018-10-20 23:23:56 -0700

    Convert BookRouter to RTL

--- src/__tests__/BookRouter.js
+++ src/__tests__/BookRouter.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { renderWithRouter } from 'test-utils';
+import rawBooks from '../data';
+import { initializeBooks } from '../helpers/helpers';
 import BookRouter from '../components/BookRouter';
 
-const UpGoingChapters = [
-  {
-    title: 'Chapter 1: Into Programming',
-    url: '/ch1',
-  },
-];
-
-const book = {
-  url: '/up-going',
-  title: 'Up & Going',
-  id: 'upGoing',
-  chapters: UpGoingChapters,
-};
+const books = initializeBooks(rawBooks);
 
 it('should render book chapters', () => {
-  const comp = shallow(<BookRouter book={book} />);
-  expect(comp.find('h2').length).toBe(1);
-  expect(
-    comp
-      .find('h2')
-      .at(0)
-      .text()
-  ).toBe('Up & Going');
-  expect(
-    comp
-      .find('Route')
-      .at(0)
-      .prop('path')
-  ).toBe('/up-going');
-  expect(
-    comp
-      .find('Route')
-      .at(1)
-      .prop('path')
-  ).toBe('/up-going/ch1');
+  const [book] = books;
+  const { getByText } = renderWithRouter(<BookRouter book={book} />, {
+    route: book.url,
+  });
+  expect(getByText(book.title)).toBeTruthy();
+  book.chapters.forEach(chapter => {
+    expect(getByText(chapter.title)).toBeTruthy();
+  });
 });
