ddf6ebea1a9b2cf2020dc93e7cd3dde01c3aa7eb
austintackaberry <austintackaberry@gmail.com>
2018-10-20 23:29:26 -0700

    Convert BookHome to RTL

--- src/__tests__/BookHome.js
+++ src/__tests__/BookHome.js
@@ -1,59 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { renderWithRouter } from 'test-utils';
 import BookHome from '../components/BookHome';
+import rawBooks from '../data';
+import { initializeBooks } from '../helpers/helpers';
 
-const UpGoingChapters = [
-  {
-    title: 'Chapter 1: Into Programming',
-    url: '/ch1',
-  },
-  {
-    title: 'Chapter 2: Into JavaScript',
-    url: '/ch2',
-  },
-];
-
-const book = {
-  url: '/up-going',
-  title: 'Up & Going',
-  id: 'upGoing',
-  chapters: UpGoingChapters,
-};
+const books = initializeBooks(rawBooks);
 
 it('should render the book chapters', () => {
-  const comp = shallow(<BookHome book={book} />);
-  expect(comp.find('List').length).toBe(1);
-  expect(comp.find('Link').length).toBe(2);
-  expect(
-    comp
-      .find('Link')
-      .at(0)
-      .prop('to')
-  ).toBe('/up-going/ch1');
-  expect(
-    comp
-      .find('Link')
-      .at(0)
-      .prop('to')
-  ).toBe('/up-going/ch1');
-  expect(comp.find('ListItem').length).toBe(2);
-  expect(
-    comp
-      .find('ListItem')
-      .at(0)
-      .children()
-      .at(0)
-      .children()
-      .text()
-  ).toBe('Chapter 1: Into Programming');
-  expect(
-    comp
-      .find('ListItem')
-      .at(1)
-      .children()
-      .at(0)
-      .children()
-      .text()
-  ).toBe('Chapter 2: Into JavaScript');
+  const [book] = books;
+  const { getByText } = renderWithRouter(<BookHome book={book} />);
+  book.chapters.forEach(chapter => {
+    expect(getByText(chapter.title)).toBeTruthy();
+  });
 });
