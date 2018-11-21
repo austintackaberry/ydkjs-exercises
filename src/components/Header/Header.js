e637452a6e14f854fc6c4d9b15ec9d6dd49bc9e8
oliver <oliver.jugen90@gmail.com>
2018-11-21 23:10:29 +0800

    changed header title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <HeaderGridChild data-name="Header">
       <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
-        <Marquee>{`YDKJS EXERCISES`}</Marquee>
+        <Marquee>{`You Don't Know JS Exercises`}</Marquee>
       </Link>
     </HeaderGridChild>
   );
