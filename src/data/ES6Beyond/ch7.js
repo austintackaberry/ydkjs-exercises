831df7c418748d9536fd67dfb4293b895550f4e0
Asem <asem.mahmod@gmail.com>
2020-09-14 13:15:20 +0200

    book6, ch7: 5 new questions

--- src/data/ES6Beyond/ch7.js
+++ src/data/ES6Beyond/ch7.js
@@ -179,6 +179,146 @@ ${'```'}
     explanation:
       '`toPrimitive` is used when an object must be coerced to a primitive value. We are overriding its typical functionality for `arr` and forcing it to be `5`.',
   },
+  {
+    question: `What will be logged to the console when the following code is executed?
+  ${'```js'}
+  let obj = {a: 1, b: 2};
+  let handlers = {
+    get(target,key,context) {
+      if (Reflect.has( target, key )) {
+        return Reflect.get(target, key, context);
+      }
+      else {
+        throw "No such property/method!";
+      }
+    }
+  }
+  let proxy = new Proxy(obj, handlers);
+
+  console.log(proxy.c);
+  ${'```'}  
+    `,
+    questionId: 'cjmyfH1aq9eUOz8eIhzn',
+    shouldBeRandomized: true,
+    answers: [
+      {
+        text: 'No such property/method!',
+        id: 0
+      },
+      {
+        text: '`undefined`',
+        id: 1
+      },
+      {
+        text: '`null`',
+        id: 2
+      }
+    ],
+    correctAnswerId: 0,
+    moreInfoUrl: 'https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/es6%20%26%20beyond/ch7.md#no-such-propertymethod',
+    explanation: 'A proxy is a special kind of object you create that "wraps" -- or sits in front of -- another normal object. You can register special handlers (aka traps) on the proxy object which are called when various operations are performed against the proxy. These handlers have the opportunity to perform extra logic in addition to forwarding the operations on to the original target/wrapped object.'
+  },
+  {
+    question: `The following code is a valid way for testing whether arrow functions work in the runtime environment:
+  ${'```js'}
+  try {
+    a = () => {};
+    ARROW_FUNCS_ENABLED = true;
+  }
+  catch (err) {
+    ARROW_FUNCS_ENABLED = false;
+  }
+  ${'```'}  
+    `,
+    questionId: 'JdSNTlTUiF80BflaNC0f',
+    shouldBeRandomized: false,
+    answers: [
+      {
+        text: 'True',
+        id: 0
+      },
+      {
+        text: 'False',
+        id: 1
+      }
+    ],
+    correctAnswerId: 1,
+    moreInfoUrl: 'https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/es6%20%26%20beyond/ch7.md#feature-testing',
+    explanation: "Unfortunately, this doesn't work, because our JS programs are compiled. Thus, the engine will choke on the arrow function syntax if it doesn't already support it. The alternative would be to use `new Function( \"(() => { })\" );` inside the `try` block."
+  },
+  {
+    question: 'Tail Call Optimization helps us save memory when we do object oriented programming',
+    questionId: 'VPNZuLaq6sqxaT5NwfrG',
+    shouldBeRandomized: false,
+    answers: [
+      {
+        text: 'True',
+        id: 0
+      },
+      {
+        text: 'False',
+        id: 1
+      }
+    ],
+    correctAnswerId: 1,
+    moreInfoUrl: 'https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/es6%20%26%20beyond/ch7.md#tail-call-optimization-tco',
+    explanation: "Certain patterns of function calls in recursive programming, called tail calls, can be optimized in a way to avoid the extra allocation of stack frames. Instead of creating a new stack frame for the next function call, the engine just reuses the existing stack frame. That works because a function doesn't need to preserve any of the current state."
+  },
+  {
+    question: `Tail Call Optimization will be applied to the following code:
+  ${'```js'}
+  "use strict";
+  function foo(x) {
+    return x * 2;
+  }
+  
+  function bar(x) {
+    x = x + 1;
+    if (x > 10) {
+      return foo( x );
+    }
+    else {
+      return bar( x + 1 );
+    }
+  }
+  
+  bar( 5 );
+  ${'```'}  
+    `,
+    questionId: '8YXGfLiswgEp2Z9Zlz1t',
+    shouldBeRandomized: false,
+    answers: [
+      {
+        text: 'True',
+        id: 0
+      },
+      {
+        text: 'False',
+        id: 1
+      }
+    ],
+    correctAnswerId: 0,
+    moreInfoUrl: 'https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/es6%20%26%20beyond/ch7.md#tail-call-optimization-tco',
+    explanation: "`foo(..)` and `bar(..)` both are in tail position, as they're the last thing to happen in their code path (other than the return). In both cases, the function calls are in proper tail position. Proper Tail Calls (PTC) of these forms can be optimized."
+  },
+  {
+    question: 'Tail Call Optimization (TCO) can only be applied in strict mode',
+    questionId: 'zs1gBtihXhZQVHgOkY75',
+    shouldBeRandomized: false,
+    answers: [
+      {
+        text: 'True',
+        id: 0
+      },
+      {
+        text: 'False',
+        id: 1
+      }
+    ],
+    correctAnswerId: 0,
+    moreInfoUrl: 'https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/es6%20%26%20beyond/ch7.md#tail-call-optimization-tco',
+    explanation: "This optimization can only be applied in strict mode. Yet another reason to always be writing all your code as strict!"
+  }
 ];
 
 export default Ch7Questions;
